bce7bcf275bddf5ca7af41a881103ecce808e6dc
CelineCambier <cambier.celine2@gmail.com>
2023-07-16 15:25:16 -0700

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import Weather from "./Weather.js";
 import Footer from "./Footer.js";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
        <h1>Weather App</h1>
        <Weather city="Miami"/>
         <Footer/>
-      </header>
     </div>
   );
 }
 
-export default App;
