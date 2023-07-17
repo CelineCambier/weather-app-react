6ccfe804ba96c1355f28b0236da8bc265fea97c5
CelineCambier <cambier.celine2@gmail.com>
2023-07-16 18:05:44 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
 import Weather from "./Weather.js";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-       <h1>Weather App</h1>
-       <Weather city="Miami"/>
-         <footer>
-        This project was coded by
-        <span className="coder">
-          <strong> Celine Cambier</strong> and is{" "}
-          <a
-            href="https://github.com/CelineCambier/weather-app-react"
-            className="github-link"
-            target="_blank"
-            rel="noreferrer"
-          >Open-Sourced on GitHub
-          </a>
-          .
-        </span>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <Weather city="Miami" />
+        <footer>
+          This project was coded by
+          <span className="coder">
+            <strong> Celine Cambier</strong> and is{" "}
+            <a
+              href="https://github.com/CelineCambier/weather-app-react"
+              className="github-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-Sourced on GitHub
+            </a>
+            .
+          </span>
+        </footer>
+      </div>
     </div>
   );
 }
-
