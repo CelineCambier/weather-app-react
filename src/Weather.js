31931e302d169970c5490f30e5d3e610fafad501
CelineCambier <cambier.celine2@gmail.com>
2023-07-20 20:56:15 -0700

    Added Animated weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       city: response.data.city,
       description: response.data.condition.description,
       humidity: response.data.temperature.humidity,
-      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      icon: response.data.condition.icon,
     });
   }
 
